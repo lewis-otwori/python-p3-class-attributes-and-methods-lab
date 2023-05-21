0e510348f3ade009ad7dc5f982fa927288879f7e
lewis-otwori <lewisotwori@gmail.com>
2023-05-21 11:00:25 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,84 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
+
+now_you_know = Song("now you know", "Nyashinski", "Hiphop")
+print(now_you_know.name)
+print(now_you_know.artist)
+print(now_you_know.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+goals = Song("goals", "Nyashinski", "Kenyan Hiphop")
+print(goals.name)
+print(goals.artist)
+print(goals.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+million_dollar = Song("million dollar", "Wakadinali", "Kenyan drill")
+print(million_dollar.name)
+print(million_dollar.artist)
+print(million_dollar.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
+
+love_yourz = Song("love yourz", "J-cole", "Rap")
+print(love_yourz.name)
+print(love_yourz.artist)
+print(love_yourz.genre)
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
